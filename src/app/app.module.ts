f45216e462b63a1a963cb26d285dc347186a132d
FeldmanMatan <feldi.rm@gmail.com>
2021-05-15 05:41:24 +0300

    new log in component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { HomePageAngularComponent } from './home-page-angular/home-page-angular.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    HomePageAngularComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home-page-angular/home-page-angular.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page-angular',
+  templateUrl: './home-page-angular.component.html',
+  styleUrls: ['./home-page-angular.component.css']
+})
+export class HomePageAngularComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
