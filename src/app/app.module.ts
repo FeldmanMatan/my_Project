edf20b51695badafc8fc0d926114d5683a726d53
FeldmanMatan <feldi.rm@gmail.com>
2021-05-15 16:18:59 +0300

    add component registration,admin and add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,76 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogInComponent } from './log-in/log-in.component';
-import { HomePageAngularComponent } from './home-page-angular/home-page-angular.component';
+import { RegistrationComponent} from './registration/registration.component';
+import { AdminComponent} from './admin/admin.component';
+
+import { FormsModule } from '@angular/forms';
+
+import { ReactiveFormsModule } from '@angular/forms';
+// import {RouterModule} from '@angular/router';
+import {RouterModule,ROUTES} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+
+import { from } from 'rxjs';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LogInComponent,
-    HomePageAngularComponent
+    RegistrationComponent,
+    AdminComponent,
+    
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    // NgForm,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component:RegistrationComponent
+      },
+      {
+        path: 'login',
+        component: LogInComponent
+      },
+      // {
+      //   path: '',
+      //   component:AppComponent
+      // },
+     
+      {
+        path: 'admin',
+        component:AdminComponent,
+        // canActivate:[TempServiceService]
+
+      },
+      // {
+      //   path: 'student',
+      //   component:ScreenStudentComponent
+      // },
+      // {
+      //   path: 'totur',
+      //   component:ScreenToturComponent
+      // },
+      // {
+      //   path: 'professional_details',
+      //   component:ProfessionalDetailsComponent
+      // },
+      // {
+      //   path: 'app-tabletime',
+      //   component:TabletimeComponent
+      // },
+    ]),/***/
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/registration/registration.component.ts
@@ -0,0 +1,73 @@
+
+import { Component, OnInit } from '@angular/core';
+
+// להחזיר import { TempServiceService } from '../../services/temp-service.service';
+
+
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent implements OnInit {
+
+  // student= new FormGroup({
+  //   firstName: new FormControl(''),
+  //   lastName: new FormControl(''),
+  //   password:new FormControl(''),
+  //   confirmPassword:new FormControl(''),
+  //   email: new FormControl(''),
+  //   phone:new FormControl(''),
+  // })
+
+   student= {
+    firstName: '',
+    lastName: '',
+    password:'',
+    confirmPassword:'',
+    email: '',
+    phone:'',
+  }
+
+ 
+/***public Temp: TempServiceService */
+  constructor( ) { 
+  
+  }
+
+ 
+  
+
+  // onSend(a:any){
+  
+  //   console.log(a.value);
+  //   console.log(a.valid);
+  //   console.log(this.student);
+ 
+  // }
+
+ 
+
+
+
+  ngOnInit(): void {
+  }
+
+
+  loginUser(event : any){
+
+    // event.prevetDefult();
+    const target= event.target
+    const email= target.querySelector('#email').value
+    const password= target.querySelector('#password').value
+
+    //this.Auth.getUserDetails(email, password);
+
+    console.log('email=', email);
+    console.log('password=', password);
+    }
+
+}
+
+
